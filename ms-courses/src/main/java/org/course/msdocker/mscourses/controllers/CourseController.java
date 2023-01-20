d8314cfa99d42c0e99699d2b930160c0a01383cd
Jaime Falla <jaimedfalla@gmail.com>
2023-01-20 08:40:21 -0500

    Courses microservice implemented

--- ms-courses/src/main/java/org/course/msdocker/mscourses/controllers/CourseController.java
+++ ms-courses/src/main/java/org/course/msdocker/mscourses/controllers/CourseController.java
@@ -1,2 +1,65 @@
-package org.course.msdocker.mscourses.controllers;public class CourseController {
+package org.course.msdocker.mscourses.controllers;
+
+import org.course.msdocker.mscourses.entities.Course;
+import org.course.msdocker.mscourses.services.ICourseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/v1/courses")
+public class CourseController {
+
+    @Autowired
+    private ICourseService _service;
+
+    @GetMapping
+    public List<Course> all(){
+        return _service.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> detail(@PathVariable Long id){
+        Optional<Course> userOpt = this._service.findById(id);
+        if(userOpt.isPresent()){
+            return ResponseEntity.ok(userOpt.get());
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<Course> save(@RequestBody Course course){
+        return ResponseEntity.status(HttpStatus.CREATED).body(_service.save(course));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Course> update(@RequestBody Course user,@PathVariable Long id){
+        Optional<Course> userOpt = _service.findById(id);
+        if(userOpt.isPresent()){
+            Course userDb = userOpt.get();
+            userDb.setName(user.getName());
+
+            return ResponseEntity.status(HttpStatus.CREATED).body(_service.save(userDb));
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id){
+        Optional<Course> user = _service.findById(id);
+
+        if(user.isPresent()){
+            _service.delete(id);
+            return ResponseEntity.noContent().build();
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+}
 }
--- ms-courses/src/main/java/org/course/msdocker/mscourses/services/CourseService.java
+++ ms-courses/src/main/java/org/course/msdocker/mscourses/services/CourseService.java
@@ -1,4 +1,41 @@
 package org.course.msdocker.mscourses.services;
 
-public class CourseService {
+import org.course.msdocker.mscourses.entities.Course;
+import org.course.msdocker.mscourses.repositories.ICourseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CourseService implements  ICourseService{
+
+    @Autowired
+    private ICourseRepository _repository;
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Course> getAll() {
+        return (List<Course>) _repository.findAll();
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Optional<Course> findById(Long id) {
+        return _repository.findById(id);
+    }
+
+    @Override
+    @Transactional
+    public Course save(Course course) {
+        return _repository.save(course);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Long id) {
+        _repository.deleteById(id);
+    }
 }
